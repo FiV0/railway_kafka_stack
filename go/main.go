83afcc659b8e8edb19709453d21627e03f441b39
antvirf <antti.viitala@icloud.com>
2024-10-31 11:22:19 +0800

    feat: wait command line arg to allow waiting for Kafka to come up

--- go/main.go
+++ go/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
@@ -14,6 +15,7 @@ var KAFKA_BROKER_ADDRESS = os.Getenv("KAFKA_BROKER_ADDRESS")
 func main() {
 	mode := flag.String("mode", "producer", "sets the mode of example program to run, either 'producer' or 'consumer'.")
 	targetTopic := flag.String("targetTopic", "testTopic", "sets the topic to produce/consume messages to")
+	waitTime := flag.Int("wait", 0, "waits desired number of seconds before trying to reach Kafka")
 	flag.Parse()
 
 	// Create Kafka connection configmap
@@ -28,6 +30,11 @@ func main() {
 	}
 
 	fmt.Printf("starting Kafka example in mode: %s\n", *mode)
+	if *waitTime != 0 {
+		fmt.Printf("waiting for %d seconds before trying to reach Kafka\n", *waitTime)
+		<-time.After(time.Duration(*waitTime) * time.Second)
+	}
+
 	if *mode == "producer" {
 		kafkaProducer(*targetTopic, config)
 	} else if *mode == "consumer" {
